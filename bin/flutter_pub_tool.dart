06d49e05d589800a6c7ebe462e82e9954c0148c7
trekhub <teddyomondi001@gmail.com>
2025-06-04 22:34:15 +0300

    added the pubspec parse fn

--- bin/flutter_pub_tool.dart
+++ bin/flutter_pub_tool.dart
@@ -1,16 +1,23 @@
-import 'dart:convert';
-import 'dart:io';
+
 
 import 'package:args/args.dart';
+import 'package:flutter_pub_tool/services/pubspec_parser_service.dart';
 
 void main(List<String> arguments) {
   final parser = ArgParser()
-    ..addFlag('help',abbr: 'h', negatable: false, help: 'Show usage help');
+    ..addFlag('help', abbr: 'h', negatable: false, help: 'Show usage help')
+    ..addFlag('tree', help: 'Print full dependency tree');
   final ArgResults results = parser.parse(arguments);
 
   if (results['help']) {
     print('📦 flutter_dependency_tracker');
     print(parser.usage);
     return;
+  }
+
+  if (results['tree']) {
+    final pubspecTree = PubspecParserService().parsePubspecFile('pubspec.yaml');
+    print('🧩 Dependencies: ${pubspecTree.dependencies}');
+    print('🛠 Dev Dependencies: ${pubspecTree.devDependencies}');
   }
 }
--- lib/services/pubspec_parser_service.dart
+++ lib/services/pubspec_parser_service.dart
@@ -1,1 +1,34 @@
-class PubspecParserService {}
+import 'dart:io';
+
+import 'package:flutter_pub_tool/models/pubspec_info_model.dart';
+import 'package:yaml/yaml.dart';
+
+///Parses pubspec.yaml and extracts dependencies.
+class PubspecParserService {
+  PubspecInfoModel parsePubspecFile(String path) {
+    ///Returns [PubspecInfoModel] from the given pubspec file [path].
+    ///
+    ///[Throws] if the file does not exit
+    final file = File(path);
+    if (!file.existsSync()) {
+      throw Exception('Pubspec.yaml not found  at $path');
+    }
+    final fileContent = file.readAsStringSync();
+    final yamlContent = loadYaml(fileContent);
+
+    final deps = <String>[];
+    final devDepts = <String>[];
+
+    if (yamlContent['dependencies'] != null) {
+      deps.addAll((yamlContent['dependencies'] as Map).keys.cast<String>());
+    }
+
+    if (yamlContent['dev_dependecies'] != null) {
+      devDepts.addAll(
+        (yamlContent['dev_dependencies'] as Map).keys.cast<String>(),
+      );
+    }
+
+    return PubspecInfoModel(dependencies: deps, devDependencies: devDepts);
+  }
+}
