f51f009576067ee035fa7ebec3d8ca4f7558e622
trekhub <teddyomondi001@gmail.com>
2025-06-04 22:15:52 +0300

    added the pubspec info model and service

---
+++ lib/models/pubspec_info_model.dart
@@ -0,0 +1,8 @@
+class PubspecInfoModel {
+  final List<String> dependencies;
+  final List<String> devDependencies;
+
+  PubspecInfoModel({
+    required this.dependencies, required this.devDependencies
+  });
+} 
